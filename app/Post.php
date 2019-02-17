2b97d0614dda196c260c96b9df19d50193529153
NeilSiao <NeilSiao@github.com>
2019-02-17 15:26:36 +0800

    modify post table name

--- app/Post.php
+++ app/Post.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     //Table Name
-    protected $table = 'Posts';
+    protected $table = 'posts';
     //Public Key
     public $primaryKey = 'id';
     //Timestamps
